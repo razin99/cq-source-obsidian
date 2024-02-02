bc61a933ee2158bd149f6d2600b063bd4776c4c1
Razin Idzuddin <razin99@gmail.com>
2024-02-03 10:13:44 +1100

    extract event struct out of response

--- obsidian/get_all_events.go
+++ obsidian/get_all_events.go
@@ -30,81 +30,83 @@ func (o ObsidianClient) GetAllEvents(ctx context.Context, cursor string) (*GetAl
 //go:embed gql/get_all_events.gql
 var getAllEventsQuery string
 
+type Event struct {
+	Actors []struct {
+		Id   string `json:"id"`
+		Name string `json:"name"`
+		User struct {
+			Id string `json:"id"`
+		} `json:"user"`
+		IsAdmin bool `json:"isAdmin"`
+		Email   struct {
+			Raw string `json:"raw"`
+		} `json:"email"`
+		GeneralProperties struct {
+			JobTitle string `json:"jobTitle"`
+		} `json:"generalProperties"`
+	} `json:"actors"`
+	Targets []struct {
+		Id   string `json:"id"`
+		Name string `json:"name"`
+		User struct {
+			Id string `json:"id"`
+		} `json:"user"`
+		IsAdmin bool `json:"isAdmin"`
+		Email   struct {
+			Raw string `json:"raw"`
+		} `json:"email"`
+	} `json:"targets"`
+	BrowserId                string    `json:"browserId"`
+	Datetime                 time.Time `json:"datetime"`
+	DeviceId                 string    `json:"deviceId"`
+	EventFrequency           string    `json:"eventFrequency"`
+	HumanReadableDescription struct {
+		Plain  string `json:"plain"`
+		Tagged string `json:"tagged"`
+	} `json:"humanReadableDescription"`
+	Id        string `json:"id"`
+	IPAddress struct {
+		Raw                 string `json:"raw"`
+		Isp                 string `json:"isp"`
+		Asn                 string `json:"asn"`
+		IsMobile            bool   `json:"isMobile"`
+		Hostname            string `json:"hostname"`
+		VpnProxyDescription string `json:"vpnProxyDescription"`
+		VpnProxyType        string `json:"vpnProxyType"`
+	} `json:"ipAddress"`
+	IsPrivilegedEvent bool `json:"isPrivilegedEvent"`
+	Location          struct {
+		City        string `json:"city"`
+		Country     string `json:"country"`
+		Subdivision string `json:"subdivision"`
+	} `json:"location"`
+	ObsidianEventName struct {
+		Description string `json:"description"`
+		Type        string `json:"type"`
+	} `json:"obsidianEventName"`
+	Service struct {
+		ServiceId string `json:"serviceId"`
+		Name      string `json:"name"`
+	} `json:"service"`
+	SessionId    string `json:"sessionId"`
+	TenantId     string `json:"tenantId"`
+	TenantObject struct {
+		Name     string `json:"name"`
+		TenantId string `json:"tenantId"`
+	} `json:"tenantObject"`
+	SubService string `json:"subService"`
+	Status     string `json:"status"`
+	UserAgent  struct {
+		Raw string `json:"raw"`
+	} `json:"userAgent"`
+}
+
 type GetAllEventsResponse struct {
 	Data struct {
 		GetEvents struct {
-			HasMoreResults bool   `json:"hasMoreResults"`
-			Cursor         string `json:"cursor"`
-			Results        []struct {
-				Actors []struct {
-					ID   string `json:"id"`
-					Name string `json:"name"`
-					User struct {
-						ID string `json:"id"`
-					} `json:"user"`
-					IsAdmin bool `json:"isAdmin"`
-					Email   struct {
-						Raw string `json:"raw"`
-					} `json:"email"`
-					GeneralProperties struct {
-						JobTitle string `json:"jobTitle"`
-					} `json:"generalProperties"`
-				} `json:"actors"`
-				Targets []struct {
-					ID   string `json:"id"`
-					Name string `json:"name"`
-					User struct {
-						ID string `json:"id"`
-					} `json:"user"`
-					IsAdmin bool `json:"isAdmin"`
-					Email   struct {
-						Raw string `json:"raw"`
-					} `json:"email"`
-				} `json:"targets"`
-				BrowserID                string    `json:"browserId"`
-				Datetime                 time.Time `json:"datetime"`
-				DeviceID                 string    `json:"deviceId"`
-				EventFrequency           string    `json:"eventFrequency"`
-				HumanReadableDescription struct {
-					Plain  string `json:"plain"`
-					Tagged string `json:"tagged"`
-				} `json:"humanReadableDescription"`
-				ID        string `json:"id"`
-				IPAddress struct {
-					Raw                 string `json:"raw"`
-					Isp                 string `json:"isp"`
-					Asn                 string `json:"asn"`
-					IsMobile            bool   `json:"isMobile"`
-					Hostname            string `json:"hostname"`
-					VpnProxyDescription string `json:"vpnProxyDescription"`
-					VpnProxyType        string `json:"vpnProxyType"`
-				} `json:"ipAddress"`
-				IsPrivilegedEvent bool `json:"isPrivilegedEvent"`
-				Location          struct {
-					City        string `json:"city"`
-					Country     string `json:"country"`
-					Subdivision string `json:"subdivision"`
-				} `json:"location"`
-				ObsidianEventName struct {
-					Description string `json:"description"`
-					Type        string `json:"type"`
-				} `json:"obsidianEventName"`
-				Service struct {
-					ServiceID string `json:"serviceId"`
-					Name      string `json:"name"`
-				} `json:"service"`
-				SessionID    string `json:"sessionId"`
-				TenantID     string `json:"tenantId"`
-				TenantObject struct {
-					Name     string `json:"name"`
-					TenantID string `json:"tenantId"`
-				} `json:"tenantObject"`
-				SubService string `json:"subService"`
-				Status     string `json:"status"`
-				UserAgent  struct {
-					Raw string `json:"raw"`
-				} `json:"userAgent"`
-			} `json:"results"`
+			HasMoreResults bool    `json:"hasMoreResults"`
+			Cursor         string  `json:"cursor"`
+			Results        []Event `json:"results"`
 		} `json:"getEvents"`
 	} `json:"data"`
 }
