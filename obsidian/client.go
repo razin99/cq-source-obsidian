c367beb4112eb62ab99aa33be284d5d885a856ee
Razin Idzuddin <razin99@gmail.com>
2024-02-03 11:55:27 +1100

    provide more information when http errors out

--- obsidian/client.go
+++ obsidian/client.go
@@ -50,8 +50,21 @@ func (o ObsidianClient) NewRequest(ctx context.Context, operation GqlOperation) 
 		return nil, err
 	}
 
+	type Error struct {
+		Message    string `json:"message"`
+		Extensions struct {
+			Code string `json:"code"`
+		} `json:"extensions"`
+	}
+
 	if res.StatusCode != 200 {
-		return nil, fmt.Errorf("[%s] - %s", operation.OperationName, res.Status)
+		var fail struct {
+			Errors []Error `json:"errors"`
+		}
+		if err := json.NewDecoder(res.Body).Decode(&fail); err != nil {
+			return nil, fmt.Errorf("[%s] - %s", operation.OperationName, res.Status)
+		}
+		return nil, fmt.Errorf("[%s] - %s - %v", operation.OperationName, res.Status, fail)
 	}
 
 	return res, nil
