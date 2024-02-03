a0680b08edb36879426c6c2f9459b821be54372d
Razin Idzuddin <razin99@gmail.com>
2024-02-03 11:55:58 +1100

    extract PostureRule struct

--- obsidian/list_global_posture_rules.go
+++ obsidian/list_global_posture_rules.go
@@ -30,61 +30,63 @@ func (o ObsidianClient) ListGlobalPostureRules(ctx context.Context, cursor strin
 //go:embed gql/list_global_posture_rules.gql
 var listGlobalPostureRulesQuery string
 
+type PostureRule struct {
+	RuleId         string   `json:"rule_id"`
+	PlatformId     string   `json:"platform_id"`
+	ProductIds     []string `json:"product_ids"`
+	SecurityDomain struct {
+		Id   string `json:"id"`
+		Name string `json:"name"`
+	} `json:"security_domain"`
+	Name                    string   `json:"name"`
+	RiskLevel               string   `json:"risk_level"`
+	StandardIds             []string `json:"standard_ids"`
+	ControlIds              []string `json:"control_ids"`
+	ObsidianRule            bool     `json:"obsidian_rule"`
+	ReleaseLabel            string   `json:"release_label"`
+	DefaultRiskLevel        string   `json:"default_risk_level"`
+	BenchmarkValueType      string   `json:"benchmark_value_type"`
+	RuleValueType           string   `json:"rule_value_type"`
+	Description             string   `json:"description"`
+	RemediationInstructions string   `json:"remediation_instructions"`
+	ExceptionsCount         struct {
+		Active   uint64 `json:"active"`
+		Inactive uint64 `json:"inactive"`
+	} `json:"exceptions_count"`
+	TenantStates []struct {
+		TenantId   string `json:"tenant_id"`
+		IsPassing  bool   `json:"is_passing"`
+		Violations uint64 `json:"violations"`
+		Tenant     struct {
+			TenantId     string `json:"tenantId"`
+			Name         string `json:"name"`
+			ServiceId    string `json:"serviceId"`
+			IsProduction bool   `json:"isProduction"`
+			Sensitivity  struct {
+				DisplayName string `json:"displayName"`
+				Enum        string `json:"enum"`
+			} `json:"sensitivity"`
+			Platform string `json:"platform"`
+		} `json:"tenant"`
+		LastScanned     time.Time `json:"last_scanned"`
+		RiskAccepted    bool      `json:"risk_accepted"`
+		ExceptionsCount struct {
+			Active   uint64 `json:"active"`
+			Inactive uint64 `json:"inactive"`
+		} `json:"exceptions_count"`
+		CorrectionScoreChange float64 `json:"correction_score_change"`
+	} `json:"tenant_states"`
+	TotalViolations       uint64  `json:"total_violations"`
+	CorrectionScoreChange float64 `json:"correction_score_change"`
+}
+
 type ListGlobalPostureRulesResponse struct {
 	Data struct {
 		ListGlobalPostureRules struct {
-			HasMoreResults bool   `json:"has_more_results"`
-			Cursor         string `json:"cursor"`
-			Rules          []struct {
-				RuleID         string   `json:"rule_id"`
-				PlatformID     string   `json:"platform_id"`
-				ProductIds     []string `json:"product_ids"`
-				SecurityDomain struct {
-					ID   string `json:"id"`
-					Name string `json:"name"`
-				} `json:"security_domain"`
-				Name                    string   `json:"name"`
-				RiskLevel               string   `json:"risk_level"`
-				StandardIds             []string `json:"standard_ids"`
-				ControlIds              []string `json:"control_ids"`
-				ObsidianRule            bool     `json:"obsidian_rule"`
-				ReleaseLabel            string   `json:"release_label"`
-				DefaultRiskLevel        string   `json:"default_risk_level"`
-				BenchmarkValueType      string   `json:"benchmark_value_type"`
-				RuleValueType           string   `json:"rule_value_type"`
-				Description             string   `json:"description"`
-				RemediationInstructions string   `json:"remediation_instructions"`
-				ExceptionsCount         struct {
-					Active   uint64 `json:"active"`
-					Inactive uint64 `json:"inactive"`
-				} `json:"exceptions_count"`
-				TenantStates []struct {
-					TenantID   string `json:"tenant_id"`
-					IsPassing  bool   `json:"is_passing"`
-					Violations uint64 `json:"violations"`
-					Tenant     struct {
-						TenantID     string `json:"tenantId"`
-						Name         string `json:"name"`
-						ServiceID    string `json:"serviceId"`
-						IsProduction bool   `json:"isProduction"`
-						Sensitivity  struct {
-							DisplayName string `json:"displayName"`
-							Enum        string `json:"enum"`
-						} `json:"sensitivity"`
-						Platform string `json:"platform"`
-					} `json:"tenant"`
-					LastScanned     time.Time `json:"last_scanned"`
-					RiskAccepted    bool      `json:"risk_accepted"`
-					ExceptionsCount struct {
-						Active   uint64 `json:"active"`
-						Inactive uint64 `json:"inactive"`
-					} `json:"exceptions_count"`
-					CorrectionScoreChange float64 `json:"correction_score_change"`
-				} `json:"tenant_states"`
-				TotalViolations       uint64  `json:"total_violations"`
-				CorrectionScoreChange float64 `json:"correction_score_change"`
-			} `json:"rules"`
-			Total uint64 `json:"total"`
+			HasMoreResults bool          `json:"has_more_results"`
+			Cursor         string        `json:"cursor"`
+			Rules          []PostureRule `json:"rules"`
+			Total          uint64        `json:"total"`
 		} `json:"listGlobalPostureRules"`
 	} `json:"data"`
 }
